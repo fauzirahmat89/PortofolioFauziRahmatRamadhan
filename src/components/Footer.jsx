bbd43fb6071e3fa7c749044d46feea9bcc3d07ab
fauzirahmat89 <fauzirahmat89@gmail.com>
2025-02-18 17:04:32 +0700

    fix mobile view

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,12 @@ const Footer = () => {
         <div class="max-w-7xl mx-auto px-6">
           <div class="flex justify-between">
             <div class="w-full sm:w-1/3 mb-6 sm:mb-0">
-              <h3 class="text-xl font-semibold">About Us</h3>
-              <p class="mt-2 text-black text-justify">Saya adalah seorang website developer yang berkomitmen untuk terus belajar dan mengembangkan skill saya.</p>
+              <h3 class="text-[2vw] font-semibold">About Us</h3>
+              <p class="mt-2 text-black text-justify text-[1vw]">Saya adalah seorang website developer yang berkomitmen untuk terus belajar dan mengembangkan skill saya.</p>
             </div>
             <div class=" mb-6 sm:mb-0">
-              <h3 class="text-xl font-semibold">Quick Links</h3>
-              <ul class="mt-2 text-gray-400">
+              <h3 class="text-[1.2vw] font-semibold">Quick Links</h3>
+              <ul class="mt-2 text-gray-400 text-[0.9vw]">
                 <li><a href="#" class="hover:text-black">Beranda</a></li>
                 <li><a href="#AboutMe" class="hover:text-black">Tentang Saya</a></li>
                 <li><a href="#Skill" class="hover:text-black">Skill</a></li>
@@ -21,8 +21,8 @@ const Footer = () => {
               </ul>
             </div>
             <div class="">
-              <h3 class="text-xl font-semibold">Follow Me</h3>
-              <div class="flex space-x-4 mt-2">
+              <h3 class="text-[1.2vw] font-semibold">Follow Me</h3>
+              <div class="flex space-x-4 mt-2 text-[0.9vw]">
                 <a href="https://github.com/fauzirahmat89" class="text-gray-400 hover:text-black">Github</a>
                 <a href="https://www.instagram.com/fauzi_rahmat_ramadhan?igsh=MW1ieGQybzBiY2l3dA==" class="text-gray-400 hover:text-black">Instagram</a>
                 <a href="https://www.linkedin.com/in/fauzi-rahmat-ramadhan-a608331a9/" class="text-gray-400 hover:text-black">LinkedIn</a>
